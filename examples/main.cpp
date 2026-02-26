c5ec145d8fc31a1cdb94093ea8fbc9afbb5f855a
vAayush3301 <vaayush3301@gmail.com>
2026-02-26 17:17:51 +0530

    OpenGL setup

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,57 +1,42 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include "library/math/vec4.h"
-#include "library/math/transform.h"
-#include "library/camera/Camera.h"
 
-using namespace gamelib::math;
-using namespace gamelib::camera;
+int main()
+{
+    if (!glfwInit()) {
+        std::cout << "GLFW failed\n";
+        return -1;
+    }
 
-int main() {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    Camera cam;
-    cam.position = {0, 0, 0};
-    cam.aspect = 16.0f / 9.0f;
-    cam.fov = 3.14159f / 3.0f;   // 60 degrees
-    cam.nearPlane = 0.1f;
-    cam.farPlane = 100.0f;
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Test", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Window creation failed\n";
+        glfwTerminate();
+        return -1;
+    }
 
-    Transform t;
+    glfwMakeContextCurrent(window);
 
-    Vec4 point(1, 0, 0, 1);  // object-space point
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "GLAD failed\n";
+        return -1;
+    }
 
-    // ---- Case 1: Near object ----
-    t.position = {0, 0, -2};  // closer
-    Mat4 MVP1 = cam.getProjectionMatrix() *
-                cam.getViewMatrix() *
-                t.getModelMatrix();
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << "\n";
 
-    Vec4 clip1 = MVP1 * point;
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    clip1.x /= clip1.w;
-    clip1.y /= clip1.w;
-    clip1.z /= clip1.w;
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-    std::cout << "Near NDC: ("
-              << clip1.x << ", "
-              << clip1.y << ", "
-              << clip1.z << ")\n";
-
-    // ---- Case 2: Far object ----
-    t.position = {0, 0, -10};  // farther
-    Mat4 MVP2 = cam.getProjectionMatrix() *
-                cam.getViewMatrix() *
-                t.getModelMatrix();
-
-    Vec4 clip2 = MVP2 * point;
-
-    clip2.x /= clip2.w;
-    clip2.y /= clip2.w;
-    clip2.z /= clip2.w;
-
-    std::cout << "Far NDC: ("
-              << clip2.x << ", "
-              << clip2.y << ", "
-              << clip2.z << ")\n";
-
+    glfwTerminate();
     return 0;
 }
