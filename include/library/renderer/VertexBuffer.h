9b3e156414a9860e3f0125adbb4ab7fc4db5b97e
vAayush3301 <vaayush3301@gmail.com>
2026-02-28 12:47:41 +0530

    VertexBuffer class

---
+++ include/library/renderer/VertexBuffer.h
@@ -0,0 +1,11 @@
+class VertexBuffer {
+public:
+    VertexBuffer(const void* data, unsigned int size);
+    ~VertexBuffer();
+
+    void Bind() const;
+    void Unbind() const;
+
+private:
+    unsigned int m_RendererID;
+};
