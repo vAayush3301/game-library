f424430192b82bddd8b6e66103c019ac43dc775c
vAayush3301 <vaayush3301@gmail.com>
2026-02-28 12:46:07 +0530

    Shader class

---
+++ include/library/renderer/Shader.h
@@ -0,0 +1,13 @@
+#include <string>
+
+class Shader {
+public:
+    Shader(const std::string& vertexPath, const std::string& fragmentPath);
+    ~Shader();
+
+    void Bind() const;
+    void Unbind() const;
+
+private:
+    unsigned int m_RendererID;
+};
