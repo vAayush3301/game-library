5e5c8bc7074842791d76adb46c966caf1b7bee04
vAayush3301 <vaayush3301@gmail.com>
2026-03-07 15:22:40 +0530

    Transformation and Mesh linking

--- include/library/renderer/Mesh.h
+++ include/library/renderer/Mesh.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include "library/math/transform.h"
 
 namespace gamelib::renderer {
     class VertexArray;
@@ -15,11 +16,16 @@ namespace gamelib::renderer {
 
         uint32_t GetIndexCount() const { return m_IndexCount; }
 
+        gamelib::math::Transform2D GetTransform() { return m_Transform; }
+        const gamelib::math::Transform2D GetTransform() const { return m_Transform; }
+
     private:
         std::unique_ptr<VertexArray> m_VAO;
         std::unique_ptr<VertexBuffer> m_VBO;
         std::unique_ptr<ElementBuffer> m_EBO;
 
         uint32_t m_IndexCount;
+
+        gamelib::math::Transform2D m_Transform;
     };
 }
